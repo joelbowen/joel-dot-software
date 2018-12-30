37cf7b378eb6eacf8762eacb0774577d4966d2a1
joelbowen <joel.michael.bowen@gmail.com>
2018-12-29 22:20:56 -0500

    chore: add Marion fonts

---
+++ src/fonts/index.js
@@ -0,0 +1,5 @@
+import MarionItalic from './Marion Italic/marion-italic.ttf'
+import MarionBold from './Marion Bold/marion-bold.ttf'
+import MarionRegular from './Marion Regular/marion-regular.ttf'
+
+export default { MarionBold, MarionItalic, MarionRegular }
