cbb58c58332439374ebad12c752f9a5ab57fddcc
joelbowen <joel.michael.bowen@gmail.com>
2018-12-29 21:48:54 -0500

    feat: move profile photo to header

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,74 +1,84 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
+import Image from 'gatsby-image'
+import { Link, graphql, StaticQuery } from 'gatsby'
 import { rhythm, scale } from '../utils/typography'
 
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props
     const rootPath = `${__PATH_PREFIX__}/`
-    let header
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: `Montserrat, sans-serif`,
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: `none`,
-              textDecoration: `none`,
-              color: `inherit`,
-            }}
-            to={`/`}
-          >
-            {title}
-          </Link>
-        </h3>
-      )
-    }
     return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        {header}
-        {children}
-        <footer>
-          © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <StaticQuery
+        query={layoutQuery}
+        render={data => (
+          <div
+            style={{
+              marginLeft: `auto`,
+              marginRight: `auto`,
+              maxWidth: rhythm(24),
+              padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            }}
+          >
+            <div style={{ display: 'flex' }}>
+              <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={data.site.siteMetadata.author}
+                style={{
+                  marginRight: rhythm(1 / 2),
+                  marginBottom: 0,
+                  minWidth: 50,
+                  borderRadius: `100%`,
+                }}
+              />
+              <h1
+                style={{
+                  ...scale(1.5),
+                  marginBottom: rhythm(1.5),
+                  marginTop: 0,
+                }}
+              >
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    textDecoration: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/`}
+                >
+                  {title}
+                </Link>
+              </h1>
+            </div>
+            {children}
+            <footer>
+              © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        )}
+      />
     )
   }
 }
+
+export const layoutQuery = graphql`
+  query layoutQuery {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      childImageSharp {
+        fixed(width: 81, height: 81) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        author
+        social {
+          twitter
+        }
+      }
+    }
+  }
+`
 
 export default Layout
