c6521b5a0ad5359507450625a2b754bfff56d5ca
joelbowen <joel.michael.bowen@gmail.com>
2019-05-18 16:57:48 -0400

    feat: use styled components in Intro

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,18 +1,43 @@
 import React from 'react'
+import styled from '@emotion/styled'
 import { rhythm } from '../utils/typography'
 
+const Intro = styled.h2`
+  text-align: center;
+  font-size: ${rhythm(3 / 4)};
+  font-weight: 400;
+  line-height: 2;
+  margin-bottom: ${rhythm(2.75)};
+`
 function Bio() {
   return (
     <div
       style={{
         display: `flex`,
-        marginBottom: rhythm(2.5),
+        flexDirection: 'column',
+        marginBottom: rhythm(2.75),
+        letterSpacing: rhythm(1 / 60),
       }}
     >
-      <p>
-        I am passionate about people, I love scalability, great user experience,
-        thoughtful security, and elegant design patterns.
-      </p>
+      <Intro>
+        <strong style={{ display: 'block' }}>
+          I am passionate about people,
+        </strong>
+        I love scalability, great user experience, thoughtful security, and
+        elegant design patterns.
+      </Intro>
+      <hr
+        style={{
+          alignSelf: 'center',
+
+          border: '1px solid rgb(225, 225, 225)',
+          display: 'block',
+          height: 0,
+          margin: 0,
+          maxWidth: '300px',
+          width: '60%',
+        }}
+      />
     </div>
   )
 }
