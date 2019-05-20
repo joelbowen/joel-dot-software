c5e37b7b7ef34d26c27b43864d96481aa52f2303
joelbowen <joel.michael.bowen@gmail.com>
2019-05-19 22:16:52 -0400

    fix: basic 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,10 +6,12 @@ import SEO from '../components/seo'
 class NotFoundPage extends React.Component {
   render() {
     return (
-      <Layout location={this.props.location}>
+      <Layout location={this.props.location} title={'404'}>
         <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div style={{ textAlign: 'center' }}>
+          <h1 style={{ marginTop: '100px' }}>Not Found</h1>
+          <p>Whoops! Looks like that page doesn't exist.</p>
+        </div>
       </Layout>
     )
   }
