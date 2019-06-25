e0fbe14b0c8a10d92e8d4eceb19399a91da7789a
joelbowen <joel.michael.bowen@gmail.com>
2019-06-25 17:40:15 +0200

    chore: update the things I like

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,10 +12,10 @@ function Skills({ currentYear, startYear }) {
         <strong>{currentYear - startYear} years</strong> of my career.
       </p>
       <p>
-        <strong>Some things I like:</strong> JavaScript, React DOM & React
-        Native, Angular, Progressive Mobile, GraphQL, Static Site Generators,
-        Headless CMS, Node.js, Serverless, Containerization, Cloud Deployment,
-        SQL/NoSql/In-Memory Databases, Linux based servers.
+        <strong>Some things I like:</strong> Software Architecture, JavaScript,
+        React, Angular, Progressive Mobile, GraphQL, Static Site Generators,
+        Headless CMS, Node.js, Type Safety, Serverless, Containerization, Cloud
+        Deployment, SQL/NoSql/In-Memory Databases, Linux based servers.
       </p>
       <p>
         I also care deeply about cross-functional teams working well together. I
