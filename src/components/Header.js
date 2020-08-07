5100c186d7ef1db835b364ffdf6d9f82c444fadc
Joel Bowen <hello@joel.software>
2020-08-07 15:28:34 -0600

    fix: navigation
    fix relative redirect issues in navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -151,8 +151,8 @@ function Navigation({ title, hideNav, location }) {
     <Nav>
       {location && location.pathname === '/' ? null : <Link to="/">Home</Link>}
       <Link to="/#skills">Skills</Link>
-      <Link to="blog">Blog</Link>
-      <Link to="talks">Talks</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/talks">Talks</Link>
     </Nav>
   )
 }
