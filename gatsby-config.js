a052c1fbcb05badc350d7342068134bdf34537a1
Joel Bowen <hello@joel.software>
2021-09-14 16:50:47 -0600

    feat(style) expand-{lg,xl,xxl} classes for content children

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1024,
+              maxWidth: 1440,
             },
           },
           {
--- src/components/_Containers.js
+++ src/components/_Containers.js
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { breakpoints } from '../utils/typography'
 
 export const PageWrapper = styled.div`
   display: flex;
@@ -9,4 +10,58 @@ export const PageWrapper = styled.div`
 export const ContentWrapper = styled.div`
   margin: 0 auto;
   max-width: 700px;
+
+  // Break out of the content-wrapper container when needed
+  .expand-lg {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: ${breakpoints.md}) {
+      padding-left: 1.1475rem;
+      padding-right: 1.1475rem;
+      margin-left: calc((-100vw + 700px) / 2);
+      margin-right: calc((-100vw + 700px) / 2);
+    }
+
+    @media (min-width: ${breakpoints.lg}) {
+      margin-left: calc(-${(breakpoints.lgVal - 700) / 2}px);
+      margin-right: calc(-${(breakpoints.lgVal - 700) / 2}px);
+    }
+  }
+
+  // Break out of the content-wrapper container when needed
+  .expand-xl {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: ${breakpoints.md}) {
+      padding-left: 1.1475rem;
+      padding-right: 1.1475rem;
+      margin-left: calc((-100vw + 700px) / 2);
+      margin-right: calc((-100vw + 700px) / 2);
+    }
+
+    @media (min-width: ${breakpoints.xl}) {
+      margin-left: calc(-${(breakpoints.xlVal - 700) / 2}px);
+      margin-right: calc(-${(breakpoints.xlVal - 700) / 2}px);
+    }
+  }
+
+  // Break out of the content-wrapper container when needed
+  .expand-xxl {
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: ${breakpoints.md}) {
+      padding-left: 1.1475rem;
+      padding-right: 1.1475rem;
+      margin-left: calc((-100vw + 700px) / 2);
+      margin-right: calc((-100vw + 700px) / 2);
+    }
+
+    @media (min-width: ${breakpoints.xxl}) {
+      margin-left: -${(breakpoints.xxlVal - 700) / 2}px;
+      margin-right: -${(breakpoints.xxlVal - 700) / 2}px;
+    }
+  }
 `
